9350a7222264dfcf5baad76a2f1de866daadf7f0
maseology <mason@riffle.ca>
2019-10-12 19:04:09 -0400

    additional constants added

--- climate/constants.go
+++ climate/constants.go
@@ -1,18 +1,23 @@
 package climate
 
 const (
-	SolarConstant = 1360.8 // (L) 1360.8 ± 0.5  W/m²
+	SolarConstant = 1360.8 // (L) 1360.8 ± 0.5 W/m²
 
-	StefanBoltzmann = 5.670374419e-8 // (sigma) W m-2 K-4
+	StefanBoltzmann = 5.670374419e-8 // (sigma) [W m-2 K-4]
 
-	EarthTemperature = 284.42  // K
-	EarthEmissivity  = 1.      // assumed close to black body (as is for most solids and liquids)
-	EarthAlbedo      = .3      // (alpha)
-	EarthRadius      = 6371.e3 // m
+	EarthTemperature  = 284.42  // [K]
+	EarthEmissivity   = 1.      // assumed close to black body (as is for most solids and liquids)
+	EarthAlbedo       = .3      // (alpha)
+	EarthRadius       = 6371.e3 // [m]
+	EarthCO2Degassing = 7.5e12  // [mol K yr-1]
 
-	AtmosLapseRate = 6.   // K/km (10. for dry air)
-	AtmosHeight    = 15.  // stratospheric height km
-	AtmosCO2       = 400. // concentration of CO2 ppm
-	AtmosCH4       = 1.7  // concentration of methane ppm
-	AtmosRH        = .8   // relative humidity
+	AtmosLapseRate = 6.   // [K/km] (10. for dry air)
+	AtmosHeight    = 15.  // stratospheric height [km]
+	AtmosCO2       = 400. // concentration of CO2 [ppm]
+	AtmosCH4       = 1.7  // concentration of methane [ppm]
+	AtmosRH        = .8   // relative humidity [-]
+
+	CloudDropRadius = 1.e-6 // [m] seeded droplets = ~1-5μm
+
+	μm = 3. // nothing, just seeing if μ can be a variable name
 )
